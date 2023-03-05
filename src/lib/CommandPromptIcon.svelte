6d4db2f1efc6ec7b3f720ff5d9b5a56dcc162343
erictakman <68903571+erictakman@users.noreply.github.com>
2023-03-05 01:56:50 +0100

    Fixed major bug with icons missing svg path

--- src/lib/CommandPromptIcon.svelte
+++ src/lib/CommandPromptIcon.svelte
@@ -5,9 +5,9 @@
 
 <div style={"font-size: " + size + "px;"}>
 	<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path d="M6 4H18C19.6569 4 21 5.34315 21 7V17C21 18.6569 19.6569 20 18 20H6C4.34315 20 3 18.6569 3 17V7C3 5.34315 4.34315 4 6 4Z" stroke="black" stroke-width="2"/>
-<path d="M12 12H15" stroke="black" stroke-width="2" stroke-linecap="round"/>
-<path d="M7 12L9 10L7 8" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M6 4H18C19.6569 4 21 5.34315 21 7V17C21 18.6569 19.6569 20 18 20H6C4.34315 20 3 18.6569 3 17V7C3 5.34315 4.34315 4 6 4Z" stroke="black" stroke-width="2"></path>
+		<path d="M12 12H15" stroke="black" stroke-linecap="round" stroke-width="2"></path>
+		<path d="M7 12L9 10L7 8" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
 	</svg>
 </div>
 
--- src/lib/TableIcon.svelte
+++ src/lib/TableIcon.svelte
@@ -5,7 +5,7 @@
 
 <div style={"font-size: " + size + "px;"}>
 	<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		
+		<path clip-rule="evenodd" d="M3 20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V5C21 4.44772 20.5523 4 20 4H4C3.44772 4 3 4.44772 3 5V20ZM5 9V6H11V9H5ZM13 14V11H19V14H13ZM11 19V16H5V19H11ZM5 14V11H11V14H5ZM13 16V19H19V16H13ZM13 6V9H19V6H13Z" fill={color} fill-rule="evenodd"></path>
 	</svg>
 </div>
 
